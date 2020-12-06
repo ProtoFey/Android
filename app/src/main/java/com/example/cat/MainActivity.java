aefc85e6e7822b94473136559984a1ffd5c142d4
ProtoFey <79088230003@yandex.ru>
2020-12-06 20:56:18 +0500

    Another one

--- app/src/main/java/com/example/cat/MainActivity.java
+++ app/src/main/java/com/example/cat/MainActivity.java
@@ -3,26 +3,57 @@ package com.example.cat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    final Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Text.class);
-                startActivity(intent);
+                randomMethod();
             }
         });
+    }
+
+    public void randomMethod() {
+        int i = random.nextInt(10);
+        if (i == 0) {
+            toast();
+        }
+        if (i != 0) {
+            toText();
+        }
+    }
+
+    public void toText() {
+        Intent intent = new Intent(MainActivity.this, Text.class);
+        startActivity(intent);
+    }
+    public void toast() {
+        Toast toast = Toast.makeText(getApplicationContext(),"Ты чего наделал", Toast.LENGTH_LONG);
+        LinearLayout toastContainer = (LinearLayout) toast.getView();
+        toastContainer.setBackgroundColor(Color.TRANSPARENT);
+        ImageView catImageView = new ImageView(getApplicationContext());
+        catImageView.setImageResource(R.drawable.like_smile);
+        toastContainer.addView(catImageView, 0);
+        toast.show();
     }
 
     @Override
--- app/src/main/java/com/example/cat/Text.java
+++ app/src/main/java/com/example/cat/Text.java
@@ -1,13 +1,84 @@
 package com.example.cat;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Text extends Activity {
 
+    ArrayList<String> list = new ArrayList<>();
+    final Random random = new Random();
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.text_activity);
+        list.add("Вам сегодня повезёт");
+        list.add("Вам сегодня не повезёт");
+        list.add("Вам повiстка");
+        list.add("Неделя пройдёт,\nвыпустите её.");
+        list.add("Через год вы станете на год старше");
+        list.add("Вам сегодня повезёт");
+        list.add("Ага, попався");
+        list.add("Через год вы станете на год старше");
+        list.add("Вам сегодня повезёт");
+        list.add("Вам сегодня не повезёт");
+        list.add("Ага, попався");
+        list.add("Закажите роллов, поешьте");
+        list.add("Через год вы станете на год старше");
+        list.add("Вам БАН, откройте");
+        list.add("Вам сегодня повезёт");
+        list.add("Вам повiстка");
+        list.add("Вам сегодня повезёт");
+        list.add("Завтра вас ждёт не ждёт");
+        list.add("Закажите роллов, поешьте");
+        list.add("В чём смылс жизни?\nИскать чужие ошибки??");
+        list.add("Ага, попався");
+        list.add("Вам сегодня повезёт");
+        list.add("Через год вы станете на год старше");
+        list.add("Вам сегодня повезёт");
+        list.add("Вам сегодня не повезёт");
+        list.add("Через год вы станете на год старше");
+        list.add("Вам сегодня повезёт");
+        list.add("Вы уже умерли, просто не знаете об этом...");
+        list.add("Вам сегодня повезёт");
+        list.add("Закажите роллов, поешьте");
+        list.add("Ага, попався");
+        list.add("Вам сегодня повезёт");
+        list.add("Вам БАН, откройте");
+        list.add("Вам сегодня не повезёт");
+        list.add("Через год вы станете на год старше");
+        list.add("Через год вы станете на год старше");
+        list.add("Закажите роллов, поешьте");
+        list.add("Вам сегодня не повезёт");
+        list.add("Вам сегодня повезёт");
+        list.add("Через год вы станете на год старше");
+        list.add("Вам повiстка");
+        list.add("Вам сегодня повезёт");
+        list.add("Вам сегодня не повезёт");
+        list.add("Вам сегодня повезёт");
+        list.add("Вам БАН, откройте");
+        list.add("Вам повiстка");
+        list.add("Вам сегодня повезёт");
+        list.add("Через год вы станете на год старше");
+        list.add("Посмотри во что ты привратился");
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(list.get(random.nextInt(list.size())));
+
+        Button button = (Button) findViewById(R.id.textButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Text.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
